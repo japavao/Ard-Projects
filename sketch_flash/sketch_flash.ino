1c0ba08ae6796791ee2cadd3a90af52212e8ce38
Joseph Pavao <joe@japavao.com>
2012-07-10 21:51:40 -0700

    checking in base file

---
+++ sketch_flash/sketch_flash.ino
@@ -0,0 +1,16 @@
+
+
+const int LED = 13;
+
+void setup(){
+  pinMode(LED, OUTPUT);
+}
+
+void loop(){
+for i in 
+  digitalWrite(LED,HIGH);
+  delay(1);
+  digitalWrite(LED, LOW);
+  delay(1);
+  
+}
