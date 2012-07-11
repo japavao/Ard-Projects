24935da35d001fc9abae153b053078309e1555bf
Joseph Pavao <joe@japavao.com>
2012-07-10 22:03:39 -0700

    changed some variables

--- sketch_flash/sketch_flash.ino
+++ sketch_flash/sketch_flash.ino
@@ -1,16 +1,17 @@
 
 
 const int LED = 13;
+const int ON = 1000;
+const int OFF = 250;
 
 void setup(){
   pinMode(LED, OUTPUT);
 }
 
 void loop(){
-for i in 
   digitalWrite(LED,HIGH);
-  delay(1);
+  delay(ON);
   digitalWrite(LED, LOW);
-  delay(1);
+  delay(OFF);
   
 }
